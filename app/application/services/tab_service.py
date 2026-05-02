1c5f8e1c726a3768f20c52341936d5bd1f7b8967
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-02 11:58:33 -0300

    feat(tab-service): add create_tab method

--- app/application/services/tab_service.py
+++ app/application/services/tab_service.py
@@ -1,12 +1,36 @@
 from sqlalchemy.orm import Session
 
-from app.domain.exceptions.tab_exceptions import TabNotFoundError
+from app.api.v1.schemas.tab_schema import TabCreateSchema
+from app.domain.entities.tab_entity import TabCreateEntity
+from app.domain.exceptions.tab_exceptions import TabAlreadyExistsError, TabNotFoundError
 from app.infrastructure.database.models.tab_model import TabModel
 
 
 class TabService:
 
     # CREATE
+
+    @staticmethod
+    def create_tab(db: Session, data: TabCreateSchema):
+
+        try:
+            TabService.get_tab_by_number(db, data.number)
+            raise TabAlreadyExistsError(f"Tab {data.number} already exists.")
+
+        except TabNotFoundError:
+            pass
+
+        entity = TabCreateEntity(data.number, data.is_empty)
+
+        entity.validate()
+
+        db_model = TabModel(number=entity.number, is_empty=entity.is_empty)
+
+        db.add(db_model)
+        db.commit()
+        db.refresh(db_model)
+
+        return db_model
 
     # READ
 

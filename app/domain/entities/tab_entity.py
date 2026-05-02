a156bea4cf3f047c9cb3542591ff058b50397eb4
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-02 10:53:19 -0300

    feat(tab-entity): add is_empty field to TabCreateEntity

--- app/domain/entities/tab_entity.py
+++ app/domain/entities/tab_entity.py
@@ -6,6 +6,7 @@ from app.domain.validators.base_validator import BaseValidator
 @dataclass
 class TabCreateEntity:
     number: int
+    is_empty: bool = True
 
     def validate(self) -> None:
         self.number = BaseValidator.validate_positive_int(self.number, "number")

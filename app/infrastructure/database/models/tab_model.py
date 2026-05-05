b4465c4d20621312a2dd1da83427197c6f4a7835
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-05 19:35:28 -0300

    fix(tab-model): correct id and number column definitions for proper indexing and uniqueness

--- app/infrastructure/database/models/tab_model.py
+++ app/infrastructure/database/models/tab_model.py
@@ -8,8 +8,8 @@ from app.infrastructure.database.database import Base
 class TabModel(Base):
     __tablename__ = "tabs"
 
-    id = Column(Integer, primary_key=True, index=True)
-    number = Column(Integer, unique=True)
+    id = Column(Integer, primary_key=True)
+    number = Column(Integer, unique=False, index=True, nullable=False)
     is_open = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
     closed_at = Column(DateTime, nullable=True)

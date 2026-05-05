53764eff6fecc3f4f11183839f3fc8981b1e42f4
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-05 15:36:14 -0300

    feat(tab-entity): add created_at and closed_at fields and rename is_empty to is_open

--- app/domain/entities/tab_entity.py
+++ app/domain/entities/tab_entity.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime, timezone
 
 from app.domain.validators.base_validator import BaseValidator
 
@@ -6,8 +7,12 @@ from app.domain.validators.base_validator import BaseValidator
 @dataclass
 class TabCreateEntity:
     number: int
-    is_empty: bool = True
+    is_open: bool = False
+    created_at: datetime = datetime.now(timezone.utc)
+    closed_at: None = None
 
     def validate(self) -> None:
         self.number = BaseValidator.validate_positive_int(self.number, "number")
-        self.is_empty = BaseValidator.validate_boolean(self.is_empty, "is_empty")
+        self.is_open = BaseValidator.validate_boolean(self.is_open, "is_open")
+        self.created_at = BaseValidator.validate_datetime(self.created_at, "created_at")
+        self.closed_at = BaseValidator.validate_none(self.closed_at, "closed_at")

1d8d139a46c8203787b33138c6eec51059e64282
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-05 19:36:32 -0300

    refactor(product-schema): remove barcode field from ProductBaseSchema

--- app/api/v1/schemas/product_schema.py
+++ app/api/v1/schemas/product_schema.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, Field
 
 class ProductBaseSchema(BaseModel):
     name: str = Field(max_length=100)
-    barcode: str = Field(max_length=50)
     price: float = Field(gt=0)
 
 

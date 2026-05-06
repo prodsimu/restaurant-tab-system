e30c82f09da51338d392fd42c29d8b19d81bc347
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-06 16:24:21 -0300

    refactor(tab-service): simplify close_tab_by_number using query filter for open tabs

--- app/application/services/tab_service.py
+++ app/application/services/tab_service.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from app.api.v1.schemas.tab_schema import TabCreateSchema
@@ -62,20 +63,22 @@ class TabService:
     @staticmethod
     def close_tab_by_number(db: Session, number: int) -> TabModel:
 
-        existing_tabs = db.query(TabModel).filter(TabModel.number == number).all()
+        tab = (
+            db.query(TabModel)
+            .filter(and_(TabModel.number == number, TabModel.is_open))
+            .first()
+        )
 
-        for tab in existing_tabs:
-            if tab.is_open:
+        if not tab:
+            raise TabAlreadyClosedError("No open tab with this number exists.")
 
-                tab.is_open = False
-                tab.closed_at = datetime.now(timezone.utc)
+        tab.is_open = False
+        tab.closed_at = datetime.now(timezone.utc)
 
-                db.commit()
-                db.refresh(tab)
+        db.commit()
+        db.refresh(tab)
 
-                return tab
-
-        raise TabAlreadyClosedError("No open tab with this number exists.")
+        return tab
 
     # DELETE
 

bcf6ace9fb6ceaa080a236bb9da5aa6cf56f0162
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-06 21:06:19 -0300

    feat(main): include product router and necessary imports

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
+from app.api.v1.routes.product_routes import router as product_router
 from app.api.v1.routes.tab_routes import router as tab_router
 from app.infrastructure.database.database import Base, engine
+from app.infrastructure.database.models.product_model import ProductModel
+from app.infrastructure.database.models.tab_model import TabModel
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(product_router)
 app.include_router(tab_router)

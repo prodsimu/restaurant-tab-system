7ba0cf3e2848b1c75c31efc3e8b1d047f9823c2d
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-07 12:22:59 -0300

    feat(product-service): add get_product_by_id method

--- app/application/services/product_service.py
+++ app/application/services/product_service.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 
 from app.api.v1.schemas.product_schema import ProductCreateSchema
 from app.domain.entities.product_entity import ProductCreateEntity
+from app.domain.exceptions.product_exceptions import ProductNotFoundError
+from app.infrastructure.database.models.product_model import ProductModel
 
 
 class ProductService:
@@ -23,3 +25,14 @@ class ProductService:
         db.refresh(db_model)
 
         return db_model
+
+    # READ
+
+    @staticmethod
+    def get_product_by_id(db: Session, product_id: int):
+        product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
+        if not product:
+            raise ProductNotFoundError(f"Product with id {product_id} not found")
+
+        return product

e711f391107ff579b42f87d1a3188d42bb975562
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-07 15:45:44 -0300

    feat(product-entities): add ProductUpdateEntity and refactor entities to inherit from ProductBaseEntity

--- app/domain/entities/product_entity.py
+++ app/domain/entities/product_entity.py
@@ -6,9 +6,13 @@ from app.domain.validators.base_validator import BaseValidator
 
 
 @dataclass
-class ProductCreateEntity:
+class ProductBaseEntity:
     name: str
     price: float
+
+    def __post_init__(self):
+        self.validate()
+        self.normalize()
 
     def validate(self) -> None:
         self.name = BaseValidator.validate_string(self.name, "name")
@@ -19,3 +23,17 @@ class ProductCreateEntity:
         self.name = self.name.encode("ASCII", "ignore").decode("utf-8")
         self.name = self.name.lower()
         self.name = re.sub(r"[^a-z0-9]+", "-", self.name).strip("-")
+
+
+@dataclass
+class ProductCreateEntity(ProductBaseEntity):
+    pass
+
+
+@dataclass
+class ProductUpdateEntity(ProductBaseEntity):
+    id: int
+
+    def validate(self) -> None:
+        super().validate()
+        self.id = BaseValidator.validate_positive_int(self.id, "id")

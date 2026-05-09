6a500a313f92808a014bc7941eac3c69a53adbea
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-08 21:18:16 -0300

    feat(tab-schema): add TabResponseSchema

--- app/api/v1/schemas/tab_schema.py
+++ app/api/v1/schemas/tab_schema.py
@@ -11,3 +11,13 @@ class TabCreateSchema(TabBaseSchema):
     is_open: bool = Field(default=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     closed_at: datetime | None = Field(default=None)
+
+
+class TabResponseSchema(BaseModel):
+    id: int
+    number: int
+    is_open: bool
+    created_at: datetime
+    closed_at: datetime | None
+
+    model_config = {"from_attributes": True}

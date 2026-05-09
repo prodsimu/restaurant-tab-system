1cef647f4c31bb0980509063863cf98414ab0fe9
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-08 21:17:16 -0300

    feat(tab-routes): add response model to tab route endpoints

--- app/api/v1/routes/tab_routes.py
+++ app/api/v1/routes/tab_routes.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from app.api.v1.schemas.tab_schema import TabResponseSchema
 from app.application.services.tab_service import TabService
 from app.infrastructure.database.database import get_db
 
-router = APIRouter(prefix="/tabs", tags=["Tabs"])
+router = APIRouter(tags=["Tabs"])
 
 
 # GET
 
 
-@router.get("/tabs/{number}")
+@router.get("/tabs/{number}", response_model=TabResponseSchema)
 def list_tabs_by_number(number: int, db: Session = Depends(get_db)):
 
     try:
@@ -26,7 +27,7 @@ def list_tabs_by_number(number: int, db: Session = Depends(get_db)):
 # POST
 
 
-@router.post("/tabs/{number}")
+@router.post("/tabs/{number}", response_model=TabResponseSchema)
 def open_tab_by_number(number: int, db: Session = Depends(get_db)):
     try:
 
@@ -41,7 +42,7 @@ def open_tab_by_number(number: int, db: Session = Depends(get_db)):
 # PUT
 
 
-@router.put("/tabs/{number}")
+@router.put("/tabs/{number}", response_model=TabResponseSchema)
 def close_tab_by_number(number: int, db: Session = Depends(get_db)):
     try:
         tab = TabService.close_tab_by_number(db, number)
@@ -55,7 +56,7 @@ def close_tab_by_number(number: int, db: Session = Depends(get_db)):
 # DELETE
 
 
-@router.delete("/tabs/{id}")
+@router.delete("/tabs/{id}", response_model=TabResponseSchema)
 def delete_tab_by_id(id: int, db: Session = Depends(get_db)):
     try:
         tab = TabService.delete_tab_by_id(db, id)

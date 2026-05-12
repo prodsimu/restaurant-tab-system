067bd2094c860b92a5f086bc95b3bf94428852e5
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-12 12:56:59 -0300

    feat(main): include item routes in the main application

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from app.api.v1.routes.item_routes import router as item_router
 from app.api.v1.routes.product_routes import router as product_router
 from app.api.v1.routes.tab_routes import router as tab_router
 from app.infrastructure.database.database import Base, engine
+from app.infrastructure.database.models.item_model import ItemModel
 from app.infrastructure.database.models.product_model import ProductModel
 from app.infrastructure.database.models.tab_model import TabModel
 
@@ -12,3 +14,4 @@ app = FastAPI()
 
 app.include_router(product_router)
 app.include_router(tab_router)
+app.include_router(item_router)

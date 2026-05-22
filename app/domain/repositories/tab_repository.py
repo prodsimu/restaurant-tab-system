3aadb50b0fca3a62ed0eaef28b31c8ea255f84c1
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-22 15:22:40 -0300

    feat(tab-repository): implement open_tab_by_number method in TabRepository

--- app/domain/repositories/tab_repository.py
+++ app/domain/repositories/tab_repository.py
@@ -1,9 +1,19 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 from app.infrastructure.database.models.tab_model import TabModel
 
 
 class TabRepositoryInterface(ABC):
+
+    # CREATE
+
+    @abstractmethod
+    def open_tab_by_number(
+        self, number: int, is_open: bool, created_at: datetime, closed_at: datetime
+    ) -> TabModel: ...
+
+    # READ
 
     @abstractmethod
     def get_open_tab_by_number(self, tab_number: int) -> TabModel: ...
--- app/infrastructure/database/repositories/tab_repository.py
+++ app/infrastructure/database/repositories/tab_repository.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 
 from app.domain.repositories.tab_repository import TabRepositoryInterface
@@ -7,6 +9,25 @@ from app.infrastructure.database.models.tab_model import TabModel
 class TabRepository(TabRepositoryInterface):
     def __init__(self, db: Session):
         self.db = db
+
+    # CREATE
+
+    def open_tab_by_number(
+        self, number: int, is_open: bool, created_at: datetime, closed_at: datetime
+    ) -> TabModel:
+
+        model = TabModel(
+            number=number,
+            is_open=is_open,
+            created_at=created_at,
+            closed_at=closed_at,
+        )
+
+        self.db.add(model)
+        self.db.commit()
+        self.db.refresh(model)
+
+        return model
 
     # READ
 

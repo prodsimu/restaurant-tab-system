50c07f138c4139a54c879e28e611fd73350ee64d
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-05-23 20:45:39 -0300

    feat(product-repository): implement create_product method in ProductRepository

--- app/domain/repositories/product_repository.py
+++ app/domain/repositories/product_repository.py
@@ -4,4 +4,6 @@ from app.infrastructure.database.models.product_model import ProductModel
 
 
 class ProductRepositoryInterface(ABC):
-    pass
+
+    @abstractmethod
+    def create_product(self, name: str, price: float) -> ProductModel: ...
--- app/infrastructure/database/repositories/product_repository.py
+++ app/infrastructure/database/repositories/product_repository.py
@@ -7,3 +7,12 @@ from app.infrastructure.database.models.product_model import ProductModel
 class ProductRepository(ProductRepositoryInterface):
     def __init__(self, db: Session):
         self.db = db
+
+    def create_product(self, name: str, price: float) -> ProductModel:
+        db_model = ProductModel(name=name, price=price)
+
+        self.db.add(db_model)
+        self.db.commit()
+        self.db.refresh(db_model)
+
+        return db_model

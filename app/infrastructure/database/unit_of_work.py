da0aa21d4f113fc08219e0895062dc2138651354
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-04 19:00:17 -0300

    feat(unit-of-work): implement UnitOfWork class for managing database sessions

---
+++ app/infrastructure/database/unit_of_work.py
@@ -0,0 +1,32 @@
+from typing import Callable
+
+from sqlalchemy.orm import Session
+
+from app.infrastructure.database.repositories.item_repository import ItemRepository
+from app.infrastructure.database.repositories.product_repository import (
+    ProductRepository,
+)
+from app.infrastructure.database.repositories.tab_repository import TabRepository
+
+
+class UnitOfWork:
+
+    def __init__(self, session_factory: Callable[[], Session]):
+        self.session_factory = session_factory
+
+    def __enter__(self) -> "UnitOfWork":
+        self.db: Session = self.session_factory()
+
+        self.tabs = TabRepository(self.db)
+        self.products = ProductRepository(self.db)
+        self.items = ItemRepository(self.db)
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        if exc_type is not None:
+            self.db.rollback()
+        else:
+            self.db.commit()
+
+        self.db.close()

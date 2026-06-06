46ce7c596ced81e3044ed393db953541bf764366
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-06 19:59:44 -0300

    feat(item-repository): update ItemRepository to use unit of work pattern

--- app/infrastructure/database/repositories/item_repository.py
+++ app/infrastructure/database/repositories/item_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from app.domain.repositories.item_repository import ItemRepositoryInterface
@@ -5,53 +6,55 @@ from app.infrastructure.database.models.item_model import ItemModel
 
 
 class ItemRepository(ItemRepositoryInterface):
+
     def __init__(self, db: Session):
         self.db = db
 
     # CREATE
 
     def create_item(self, tab_id: int, product_id: int, quantity: int) -> ItemModel:
-        db_model = ItemModel(tab_id=tab_id, product_id=product_id, quantity=quantity)
-
-        self.db.add(db_model)
-        self.db.commit()
-        self.db.refresh(db_model)
-
-        return db_model
+        item = ItemModel(
+            tab_id=tab_id,
+            product_id=product_id,
+            quantity=quantity,
+        )
+        self.db.add(item)
+        self.db.flush()
+        self.db.refresh(item)
+        return item
 
     # READ
 
-    def get_item_by_id(self, item_id: int) -> ItemModel:
+    def get_item_by_id(self, item_id: int) -> ItemModel | None:
         return self.db.query(ItemModel).filter(ItemModel.id == item_id).first()
 
-    def list_items_by_tab_id(self, tab_id: int) -> list[ItemModel]:
+    def get_item_by_tab_and_product(
+        self, tab_id: int, product_id: int
+    ) -> ItemModel | None:
+        return (
+            self.db.query(ItemModel)
+            .filter(
+                and_(
+                    ItemModel.tab_id == tab_id,
+                    ItemModel.product_id == product_id,
+                )
+            )
+            .first()
+        )
+
+    def list_items_by_tab(self, tab_id: int) -> list[ItemModel]:
         return self.db.query(ItemModel).filter(ItemModel.tab_id == tab_id).all()
 
     # UPDATE
 
-    def increment_quantity(self, item_id: int, quantity: int) -> ItemModel:
-        item = self.get_item_by_id(item_id)
+    def increment_quantity(self, item: ItemModel, quantity: int) -> ItemModel:
         item.quantity += quantity
-
-        self.db.commit()
+        self.db.flush()
         self.db.refresh(item)
-
-        return item
-
-    def decrement_quantity(self, item_id: int, quantity: int) -> ItemModel:
-        item = self.get_item_by_id(item_id)
-        item.quantity -= quantity
-
-        self.db.commit()
-        self.db.refresh(item)
-
         return item
 
     # DELETE
 
-    def delete_item(self, item_id: int) -> None:
-        item = self.get_item_by_id(item_id)
-
-        if item:
-            self.db.delete(item)
-            self.db.commit()
+    def delete_item(self, item: ItemModel) -> None:
+        self.db.delete(item)
+        self.db.flush()

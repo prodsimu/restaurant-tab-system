0e71d991e37028f35c292efbe83ac7145a1763f3
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-06 19:51:23 -0300

    feat(database): add expire_on_commit option to SessionLocal configuration

--- app/infrastructure/database/database.py
+++ app/infrastructure/database/database.py
@@ -8,7 +8,9 @@ engine = create_engine(
     connect_args={"check_same_thread": False},
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, expire_on_commit=False
+)
 
 Base = declarative_base()
 

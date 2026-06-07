31fd34b214e9df92a27d3ad1291259ae6e1de307
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-07 18:08:29 -0300

    refactor(database): rename DATABASE_URL to SQLALCHEMY_DATABASE_URL for consistency

--- app/infrastructure/database/database.py
+++ app/infrastructure/database/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
-    DATABASE_URL,
+    SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
 )
 

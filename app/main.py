46a403cc7a54f8700fd9feb4d26af6ec8af7dae2
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-09 13:13:56 -0300

    feat(main): configure FastAPI with app name and debug settings

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ from fastapi import FastAPI
 from app.api.v1.routes.item_routes import router as item_router
 from app.api.v1.routes.product_routes import router as product_router
 from app.api.v1.routes.tab_routes import router as tab_router
+from app.core.settings import settings
 from app.infrastructure.database.database import Base, engine
 from app.infrastructure.database.models.item_model import ItemModel
 from app.infrastructure.database.models.product_model import ProductModel
 from app.infrastructure.database.models.tab_model import TabModel
 from app.infrastructure.http.exception_handlers import register_exception_handlers
 
-app = FastAPI()
+app = FastAPI(title=settings.app_name, debug=settings.debug)
 
 register_exception_handlers(app)
 

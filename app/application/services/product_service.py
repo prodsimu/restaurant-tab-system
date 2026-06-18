d73d49b6b0eea695e82834301e6fe3f81de0fd9d
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-17 21:15:57 -0300

    refactor(product-service): migrate to unit of work pattern

--- app/application/services/product_service.py
+++ app/application/services/product_service.py
@@ -2,68 +2,67 @@ from app.api.v1.schemas.product_schema import ProductCreateSchema, ProductUpdate
 from app.domain.entities.product_entity import ProductCreateEntity, ProductUpdateEntity
 from app.domain.exceptions.product_exceptions import ProductNotFoundError
 from app.infrastructure.database.models.product_model import ProductModel
-from app.infrastructure.database.repositories.product_repository import (
-    ProductRepository,
-)
+from app.infrastructure.database.unit_of_work import UnitOfWork
 
 
 class ProductService:
 
-    def __init__(self, repo: ProductRepository):
-        self.repo = repo
+    def __init__(self, uow: UnitOfWork):
+        self.uow = uow
 
     # CREATE
 
     def create_product(self, data: ProductCreateSchema) -> ProductModel:
-
-        entity = ProductCreateEntity(**data.dict())
-
-        return self.repo.create_product(name=entity.name, price=entity.price)
+        with self.uow as uow:
+            entity = ProductCreateEntity(**data.dict())
+            return uow.products.create_product(name=entity.name, price=entity.price)
 
     # READ
 
     def list_all_products(self) -> list[ProductModel]:
-        return self.repo.list_all_products()
+        with self.uow as uow:
+            return uow.products.list_all_products()
 
     def get_product_by_id(self, product_id: int) -> ProductModel:
-        product = self.repo.get_product_by_id(product_id)
+        with self.uow as uow:
+            product = uow.products.get_product_by_id(product_id)
 
-        if not product:
-            raise ProductNotFoundError(f"Product with id {product_id} not found")
+            if not product:
+                raise ProductNotFoundError(f"Product with id {product_id} not found")
 
-        return product
+            return product
 
     # UPDATE
 
     def update_product(
         self, product_id: int, data: ProductUpdateSchema
     ) -> ProductModel:
+        with self.uow as uow:
+            entity = ProductUpdateEntity(**data.dict())
 
-        entity = ProductUpdateEntity(**data.dict())
+            update_data = {
+                k: v
+                for k, v in {
+                    "name": entity.name,
+                    "price": entity.price,
+                }.items()
+                if v is not None
+            }
 
-        update_data = {
-            k: v
-            for k, v in {
-                "name": entity.name,
-                "price": entity.price,
-            }.items()
-            if v is not None
-        }
+            updated_product = uow.products.update_product(product_id, update_data)
 
-        updated_product = self.repo.update_product(product_id, update_data)
+            if not updated_product:
+                raise ProductNotFoundError(f"Product with id {product_id} not found")
 
-        if not updated_product:
-            raise ProductNotFoundError(f"Product with id {product_id} not found")
-
-        return updated_product
+            return updated_product
 
     # DELETE
 
     def delete_product(self, product_id: int) -> dict:
+        with self.uow as uow:
+            deleted_product = uow.products.delete_product(product_id)
 
-        deleted_product = self.repo.delete_product(product_id)
-
-        if not deleted_product:
-            raise ProductNotFoundError(f"Product with id {product_id} not found")
+            if not deleted_product:
+                raise ProductNotFoundError(f"Product with id {product_id} not found")
 
         return {"message": f"Product with id {product_id} deleted successfully."}

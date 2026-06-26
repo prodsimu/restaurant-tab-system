74e05ae620d6ecfd5f941d3c97dc809b2faaa799
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-06-26 09:57:56 -0300

    feat(main): register auth router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.api.v1.routes.auth_routes import router as auth_router
 from app.api.v1.routes.item_routes import router as item_router
 from app.api.v1.routes.product_routes import router as product_router
 from app.api.v1.routes.tab_routes import router as tab_router
@@ -8,12 +9,14 @@ from app.infrastructure.database.database import Base, engine
 from app.infrastructure.database.models.item_model import ItemModel
 from app.infrastructure.database.models.product_model import ProductModel
 from app.infrastructure.database.models.tab_model import TabModel
+from app.infrastructure.database.models.user_model import UserModel
 from app.infrastructure.http.exception_handlers import register_exception_handlers
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
 
 register_exception_handlers(app)
 
+app.include_router(auth_router)
 app.include_router(product_router)
 app.include_router(tab_router)
 app.include_router(item_router)
